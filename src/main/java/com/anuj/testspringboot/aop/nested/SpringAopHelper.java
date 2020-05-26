04c1ac06ee8abcc07c83e63bc2aa36f2592559e4
Anuj Mangal <anuj.mangal@gmail.com>
2020-05-26 20:18:47 +1000

    Added Aop Around Advice

--- src/main/java/com/anuj/testspringboot/aop/nested/SpringAopHelper.java
+++ src/main/java/com/anuj/testspringboot/aop/nested/SpringAopHelper.java
@@ -1,8 +1,11 @@
 package com.anuj.testspringboot.aop.nested;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
@@ -12,5 +15,14 @@ public class SpringAopHelper {
     @Before("execution(* com.anuj.testspringboot.aop.nested.*.*(..))")
     public void beforeAopCallAnyName(JoinPoint joinPoint){
         System.out.println("In beforeAopCallAnyName method from " + joinPoint);
+    }
+
+    @Around("execution(* com.anuj.testspringboot.aop.nested.*.*(..))")
+    public void aroundAdviceAnyName(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+        System.out.println(" Time taken by " + joinPoint + " method to execute is " + timeTaken + " ms");
+
     }
 }
